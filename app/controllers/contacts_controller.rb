273965c43c63b109a0c9538734107ad39972a97b
Adam Lopez <Adamf04@gmail.com>
2016-05-18 16:39:18 -0400

    adding contact form through rails gems and a tutorial

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,11 @@
 class ContactsController < ApplicationController
 
-	class ContactsController < ApplicationController
 		def new
    		@contact = Contact.new
   		end
 
   		def create
-    		@contact = Contact.new(params[:contact])
+    		@contact = Contact.new message_params
     		@contact.request = request
 	    	if @contact.deliver
 	      	flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
@@ -15,7 +14,10 @@ class ContactsController < ApplicationController
 	      	render :new
 	    	end
   		end
-	end
 
+  		private
 
+  		def message_params
+  			params.require permit(:name, :email, :message)
+  		end
 end
